9c486222a6b62eefb06fa95710e23ddcf17556e7
cacildaraiane09 <cacilda.raiane@ufba.br>
2026-04-13 21:26:29 -0300

    Update Aluno.java

--- src/main/java/br/ufba/poo/lista2/Aluno.java
+++ src/main/java/br/ufba/poo/lista2/Aluno.java
@@ -1,27 +1,50 @@
-import java.util.Objects;
+package br.ufba.poo.lista2;
+
+/**
+ * Crie a classe `Aluno`, que possui os atributos `matricula` e `nome` (ambos do
+ * tipo `String`), além dos métodos e construtores relevantes de acordo com a
+ * seguinte especificação:
+ *
+ * - O nome do aluno pode ser alterado, mas nunca o seu número de matrícula
+ * - O número de matrícula de um aluno deve poder ser acessado por outras partes
+ * do programa
+ * - Duas instâncias de `Aluno` são consideradas iguais se possuem o mesmo número
+ * de matrícula
+ *
+ * Assuma, para este exercício, que a classe `Aluno` sempre será instanciada com um
+ * número de matrícula e um nome válidos, passados nessa ordem.
+ */
+    
 public class Aluno {
     private final String matricula;
     private String nome;
-    public Aluno(String matricula, String nome) {
+    public Aluno(String matricula, String nome){
         this.matricula = matricula;
         this.nome = nome;
     }
     public void setNome (String nome){
         this.nome = nome;
     }
-    public String getNome() {
+    public String getNome(){
         return nome;  
     }
-    public String getMatricula() {
+    public String getMatricula(){
         return matricula;
     }
+    
    @Override  
-    public boolean equals(Object outroAluno) {
+    public boolean equals(Object outroAluno){
         if (this == outroAluno)
             return true;
-        if (outroAluno == null || getClass() != outroAluno.getClass())
+        if (outroAluno == null || getClass()!= outroAluno.getClass())
             return false;
         Aluno aluno = (Aluno) outroAluno;
         return Obejects.equals(this.matricula, aluno.matricula);
     }
+
+    @Override 
+    public int hashCode() {
+        return Objects.hash(matricula);
+    }
+    
 }
